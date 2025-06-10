a0d84851ec56bb3bdc459b99c4c17f7c3e882733
Maysson <mayssonalexandre@gmai.com>
2025-06-09 22:08:22 -0400

    venv added to test

---
+++ app.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template, redirect, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Categoria(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(50), nullable=False)
+    maxima_semestre = db.Column(db.Integer, nullable=False)
+    subcategorias = db.relationship('Subcategoria', backref='categoria', lazy=True)
+
+class Subcategoria(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(50), nullable=False)
+    maxima_sub=db.Column(db.Integer, nullable=False)
+    categoria_id = db.Column(db.Integer, db.ForeignKey('categoria.id'), nullable=False)
+
+with app.app_context():
+    db.create_all()
+
+insert_categorias = [
+    Categoria(nome='Aperfeiçoamento e Enriquencimento Cultural e Esportivo', maxima_semestre=80),
+    Categoria(nome='Divulgação Científica e Inicialização à Docência', maxima_semestre=60)
+]
+
+insert_subcategorias = [
+    Subcategoria(nome='Participação como agente em atividades culturais', categoria_id=1, maxima_sub=30),
+    Subcategoria(nome='Visitas técnicas e culturais', categoria_id=1, maxima_sub=30),
+    Subcategoria(nome='Monitoria remunerada ou voluntária', categoria_id=2, maxima_sub=60),
+    Subcategoria(nome='Membro atuante em atividades Técnico-científicas', categoria_id=2, maxima_sub=30)
+]
+
+with app.app_context():
+    db.create_all()
+    if Categoria.query.count() == 0:
+        db.session.bulk_save_objects(insert_categorias)
+        db.session.commit()
+    if Subcategoria.query.count() == 0:
+        db.session.bulk_save_objects(insert_subcategorias)
+        db.session.commit()
+
+@app.route('/')
+def index():
+    categorias = Categoria.query.all()
+    subcategorias = Subcategoria.query.all()
+    return render_template('enviar_horas.html', categorias=categorias)
+
+if __name__ == '__main__':
+    app.run(debug=True)
